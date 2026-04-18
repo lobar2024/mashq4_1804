808d41624cf944f917cd469bad6867df3ced94a4
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-04-18 09:44:26 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+);
+
+INSERT INTO users (name, age)
+VALUES 
+('Ali', 20),
+('Vali', 17),
+('Sami', 25);
+
+SELECT * FROM users
+WHERE age > 18;
